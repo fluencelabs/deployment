668ab12c1d5ab84209e6a492e0da8474d3c7925a
Anatoly Laskaris <github_me@nahsi.dev>
2023-12-29 16:36:19 +0200

    Add tests

--- ansible/molecule/tests/test_default.py
+++ ansible/molecule/tests/test_default.py
@@ -6,64 +6,45 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
         os.environ["MOLECULE_INVENTORY_FILE"]).get_hosts("all")
 
 
-def test_directories(host):
-    d = host.file("/opt/molecule")
+@pytest.mark.parametrize("directory", [
+    "/opt/nox/nox-1",
+    "/opt/nox/nox-2",
+])
+def test_nox_directories(host, directory):
+    d = host.file(directory)
 
     assert d.is_directory
     assert d.exists
-    assert d.user == "molecule"
-    assert d.mode == 509
+    assert d.user == "nox"
+    assert d.mode == 493
+
+def test_nox_cleanup(host):
+    directory = host.file("/opt/nox/nox-0")
+    service = host.service("nox-@0")
+
+    assert not service.is_running
+    assert not service.is_enabled
+    assert not directory.exists
 
 
-def test_files(host):
-    f = host.file("/opt/molecule/foo")
+def test_ipfs_download(host):
+    f = host.file("/opt/nox/ipfs")
 
     assert f.is_file
     assert f.exists
-    assert f.content == b'bar'
-
-
-@pytest.mark.parametrize("package", [
-    "jq",
-    "unzip",
-    "python3-dev"
-])
-def test_is_package_is_installed(host, package):
-    package = host.package(package)
-
-    assert package.is_installed
 
 
 @pytest.mark.parametrize("service", [
-    "docker",
-    "dnsmasq"
+    "nox-@1",
+    "nox-@2",
 ])
-def test_service_is_running(host, service):
+def test_nox_is_running(host, service):
     service = host.service(service)
 
     assert service.is_running
     assert service.is_enabled
 
 
-def test_user_cleanup(host):
-    assert not host.user("deleted").exists
-
-
 def test_user_created(host):
-    assert host.user("molecule").exists
-    assert host.user("molecule").uid == 666
-    assert "docker" in host.user("molecule").groups
-
-
-def test_sysctl(host):
-    assert host.sysctl("vm.swappiness") == 1
-
-
-def test_dnsmasq_docker_bind(host):
-    assert host.socket("udp://172.17.0.1:53").is_listening
-
-
-def test_limits(host):
-    l = host.run("ulimit -Sn")
-
-    assert l.stdout == '65536\n'
+    assert host.user("nox").exists
+    assert "nox" in host.user("nox").groups

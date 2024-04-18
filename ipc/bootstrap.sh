2f10eca75e0ef06aa4d49d0b81d0ac102c07699f
Anatolios Laskaris <github_me@nahsi.dev>
2024-04-18 15:38:36 +0300

    chore: Topdown recovery (#18)
    Topdown recovery

--- ipc/bootstrap.sh
+++ ipc/bootstrap.sh
@@ -5,7 +5,7 @@
 set -euo pipefail
 
 cometbft_image="cometbft/cometbft:v0.37.x"
-fendermint_image="fluencelabs/fendermint:kras"
+fendermint_image="fluencelabs/fendermint:recover"
 
 help() {
 script_name="$(basename $0)"
